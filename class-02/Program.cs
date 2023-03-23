093b6bd9c72632a81dc52fc3567b30f6f0b9411f
Pedro <pedro.enrique1599@icloud.com>
2023-03-22 22:42:16 -0300

    hotfix

--- class-02/Program.cs
+++ class-02/Program.cs
@@ -4,55 +4,55 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Book bookDB = new Book(1, "9785523039058", "Banco de Dados", 50);
+        Book bookDB = new Book(1, "9785523039058", "Banco de Dados", 50);
 
-        // Utilities.Print("\nDados do Livro: \n" + bookDB.Id + ", " + bookDB.Isbn + ", " + bookDB.Title + ", " + bookDB.Price);
+        Utilities.Print("\nDados do Livro: \n" + bookDB.Id + ", " + bookDB.Isbn + ", " + bookDB.Title + ", " + bookDB.Price);
 
-        // Utilities.BreakLine();
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // Utilities.Print("Digite algo: ");
+        Utilities.Print("Digite algo: ");
 
-        // var input = Console.ReadLine();
+        var input = Console.ReadLine();
 
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // Utilities.Print("Algo digitado: " + input);
+        Utilities.Print("Algo digitado: " + input);
 
-        // Utilities.BreakLine();
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // // Exercício 1
-        // Utilities.Print("** Calculadora **\nOperações possíveis:\n| Soma: '+' | Subtração: '-' | Divisão: '/' | Multiplicação: '*' |\n\nInforme o cálculo desejado: ");
-        // string operation = Console.ReadLine();
+        // Exercício 1
+        Utilities.Print("** Calculadora **\nOperações possíveis:\n| Soma: '+' | Subtração: '-' | Divisão: '/' | Multiplicação: '*' |\n\nInforme o cálculo desejado: ");
+        string operation = Console.ReadLine();
 
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // Utilities.Print("Informe o primeiro número a ser calculado: ");
-        // double number1 = Convert.ToDouble(Console.ReadLine());
+        Utilities.Print("Informe o primeiro número a ser calculado: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
 
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // Utilities.Print("Informe o segundo número a ser calculado: ");
-        // double number2 = Convert.ToDouble(Console.ReadLine());
+        Utilities.Print("Informe o segundo número a ser calculado: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
 
-        // Calculator Calc_01 = new Calculator();
+        Calculator Calc_01 = new Calculator();
 
-        // var outcome = Calc_01.Process(number1, number2, operation);
+        var outcome = Calc_01.Process(number1, number2, operation);
 
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // Utilities.Print("Resultado da operação: " + outcome);
+        Utilities.Print("Resultado da operação: " + outcome);
 
-        // Utilities.BreakLine();
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
+        Utilities.BreakLine();
 
-        // //  Exercício 2
-        // Person Person_01 = new Person(22, "Ava");
-        // Utilities.Print("Nome: " + Person_01.Name + "\nIdade: " + Person_01.Age);
+        //  Exercício 2
+        Person Person_01 = new Person(22, "Ava");
+        Utilities.Print("Nome: " + Person_01.Name + "\nIdade: " + Person_01.Age);
 
-        // Utilities.BreakLine();
-        // Utilities.BreakLine();
+        Utilities.BreakLine();
+        Utilities.BreakLine();
 
         // Exercício 3
         Utilities.Print("Informe o número da conta: ");
