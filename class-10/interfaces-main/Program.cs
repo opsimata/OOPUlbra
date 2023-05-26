b3935616aaa2890537123d963d56237b9186d580
Pedro <pedro.enrique1599@icloud.com>
2023-05-26 06:05:57 -0300

    class 10 init

---
+++ class-10/interfaces-main/Program.cs
@@ -0,0 +1,28 @@
+﻿// Definindo a interface IItemEstoque
+public interface IItemEstoque
+{
+    void Vender();
+    void Devolver();
+}
+
+// Implementando a interface em uma classe
+public class Roupa : IItemEstoque
+{
+    public void Vender()
+    {
+        // Implementação específica para vender roupas
+    }
+
+    public void Devolver()
+    {
+        // Implementação específica para devolver roupas
+    }
+}
+
+// Usando a classe Roupa com a interface IItemEstoque
+public class Loja
+{
+    private List<IItemEstoque> _itensEmEstoque;
+
+    public void AdicionarItem(Roupa item); //problema de acoplamento aqui
+}
